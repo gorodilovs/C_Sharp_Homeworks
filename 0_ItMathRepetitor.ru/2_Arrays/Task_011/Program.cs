8d59e851bf3070f735d7ecbedc80927f0f241556
Sergei <gorodilov.cad@gmail.com>
2022-12-04 00:19:26 +0300

    Added Tasks 011-020

---
+++ 0_ItMathRepetitor.ru/2_Arrays/Task_011/Program.cs
@@ -0,0 +1,19 @@
+﻿// Создать массив, состоящий из троек подряд идущих
+// одинаковых элементов.
+
+int[] array = new int[20];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == 0 || i % 3 == 0)
+        array[i] = 1;
+    else if (i == 1 || (i - 1) % 3 == 0)
+        array[i] = 2;
+    else
+        array[i] = 3;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
---
+++ 0_ItMathRepetitor.ru/2_Arrays/Task_012/Program.cs
@@ -0,0 +1,16 @@
+﻿// Создать массив, который одинаково читается как слева направо,
+// так и справа налево.
+
+int[] array = new int[39];
+
+for (int i = 0; i <= array.Length / 2; i++)
+{
+    array[i] = i;
+    array[array.Length - i - 1] = i;
+
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
