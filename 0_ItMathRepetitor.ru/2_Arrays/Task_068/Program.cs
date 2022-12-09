04911e58929616699b5ff0b9f765424812d657a7
Sergei <gorodilov.cad@gmail.com>
2022-12-10 00:09:29 +0300

    Added ItMathRepetitor 068

--- 0_ItMathRepetitor.ru/2_Arrays/Task_068/Program.cs
+++ 0_ItMathRepetitor.ru/2_Arrays/Task_068/Program.cs
@@ -1,13 +1,14 @@
 ﻿// Преобразовать массив таким образом, чтобы сначала располагались
-// все элементы, модуль которых не превышает единицу, а потом – все остальные. (Без использования дополнительного масиива)
+// все элементы, модуль которых не превышает единицу, а потом – все остальные.
+// (Без использования дополнительного масcива)
 
 int capacity = 20;
-int[] arr = new int[] {2, 5, 3, 0, 0, 0, 0, 1};
+int[] arr = new int[capacity];
 Random rnd = new Random();
 int counter1 = 0;
-int counter2 = 0;
-int temp1;
+int temp1 = 0;
 int temp2 = 0;
+int temp3 = 0;
 
 void PrintArray(int[] a)
 {
@@ -18,10 +19,10 @@ foreach (int n in a)
 System.Console.WriteLine();
 }
 
-// for (int i = 0; i < arr.Length; i++)
-// {
-//     arr[i] = rnd.Next(0, 10);
-// }
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(-5, 5);
+}
 
 PrintArray(arr);
 
@@ -29,18 +30,18 @@ for (int i = 0; i < arr.Length; i++)
 {
 
     if (Math.Abs(arr[i]) == 1)
-    {
+    {       
         temp1 = arr[i];
-        counter1++;
-        for (int j = counter1; j < i; j++)
+        for (int j = counter1; j <= i; j++)
         {
-            temp2 = arr[j];
-            arr[j] = arr[j - 1];
-            arr[j + 1] = temp2;
+            temp2 = arr[j];        
+            arr[j] = temp3;
+            temp3 = temp2;
         }
-        arr[counter1 - 1] = temp1;
-
+        arr[counter1] = temp1;
+        counter1++;
     }
 
 }
+System.Console.WriteLine("Matches: " + counter1);  
 PrintArray(arr);
