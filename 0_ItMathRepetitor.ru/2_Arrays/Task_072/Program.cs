e83723193018db9e5e7a117c5a9f64904d527850
Sergei <gorodilov.cad@gmail.com>
2022-12-11 18:38:05 +0300

    Added ItMathRepetitor 072-073

--- 0_ItMathRepetitor.ru/2_Arrays/Task_072/Program.cs
+++ 0_ItMathRepetitor.ru/2_Arrays/Task_072/Program.cs
@@ -1,1 +1,80 @@
-﻿// Дана упорядоченная последовательность an чисел от 1 до N. Из копии данной последовательности bn удалили одно число, а оставшиеся перемешали. Найти удаленное число.
+﻿// Дана упорядоченная последовательность an чисел от 1 до N.
+// Из копии данной последовательности bn удалили одно число,
+// а оставшиеся перемешали. Найти удаленное число.
+// Без сортировки массива Bn.
+
+int capacity = 10;
+int[] arrOriginal = new int[capacity];
+Random rnd = new Random();
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        System.Console.Write(item + " ");
+    }
+    System.Console.WriteLine();
+}
+
+for (int i = 0; i < arrOriginal.Length; i++)
+{
+    arrOriginal[i] = rnd.Next(0, 30);
+}
+
+System.Console.WriteLine("Original array: ");
+PrintArray(arrOriginal);
+
+Array.Sort(arrOriginal);
+
+System.Console.WriteLine("Sorted original array: ");
+PrintArray(arrOriginal);
+
+
+int temp = rnd.Next(0, arrOriginal.Length);
+int[] arrModified = new int[arrOriginal.Length - 1];
+
+for (int i = 0; i < arrModified.Length; i++)
+{
+    if (i >= temp)
+        arrModified[i] = arrOriginal[i + 1];
+    else
+        arrModified[i] = arrOriginal[i];
+}
+
+System.Console.WriteLine("Modified array: ");
+PrintArray(arrModified);
+
+// Fisher–Yates shuffle!
+for (int i = arrModified.Length - 1; i >= 1; i--)
+{
+    int j = rnd.Next(i + 1);
+
+    int tmp = arrModified[j];
+    arrModified[j] = arrModified[i];
+    arrModified[i] = tmp;
+}
+
+System.Console.WriteLine("Modified shuffled array:");
+PrintArray(arrModified);
+
+int[] unique = new int[arrOriginal.Length];
+unique = arrOriginal;
+
+for (int i = 0; i < arrModified.Length; i++)
+{
+    for (int j = 0; j < unique.Length; j++)
+    {
+        if (arrModified[i] == unique[j])
+        {
+            for (int k = j; k < unique.Length - 1; k++)
+            {
+                unique[k] = unique[k + 1];
+            }
+            Array.Resize(ref unique, unique.Length - 1);
+            j = unique.Length;
+        }
+    }
+}
+
+System.Console.WriteLine("Deleted numbers are: ");
+PrintArray(unique);
--- 0_ItMathRepetitor.ru/2_Arrays/Task_073/Program.cs
+++ 0_ItMathRepetitor.ru/2_Arrays/Task_073/Program.cs
@@ -1,1 +1,65 @@
-﻿// Дан массив, в котором количество отрицательных элементов равно количеству положительным. Поменяйте местами первый отрицательный и первый положительный, второй отрицательный и второй положительный и так далее.
+﻿// Дан массив, в котором количество отрицательных элементов
+// равно количеству положительных. Поменяйте местами первый
+// отрицательный и первый положительный, второй отрицательный
+// и второй положительный и так далее.
+
+int capacity = 10;
+int[] arr = new int[capacity];
+Random rnd = new Random();
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        System.Console.Write(item + " ");
+    }
+    System.Console.WriteLine();
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(1, 10);
+}
+
+System.Console.WriteLine("Original array:");
+PrintArray(arr);
+
+
+int counter = 0;
+int index = 0;
+
+while (counter <= (arr.Length - 1)/2)
+{
+    index = rnd.Next(0, arr.Length);
+    if (arr[index] > 0)
+    {
+        arr[index] = -arr[index];
+        counter++;
+    }
+}
+
+System.Console.WriteLine("Original array with half negative numbers:");
+PrintArray(arr);
+
+counter = 0;
+int temp;
+int[] arrSwaped = new int[arr.Length];
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] < 0)
+    {
+        for (int j = counter; j < arr.Length; j++)
+        {
+            if(arr[j] >= 0)
+            {
+                arrSwaped[i] = arr[j];
+                arrSwaped[j] = arr[i];
+                j = arr.Length;
+            }
+            counter++;
+        }
+    }
+}
+
+System.Console.WriteLine("Swapped array:");
+PrintArray(arrSwaped);
